9044dc0636d6cc08f6738bb236041422dab33c6e
raffamoussi <00001099496809sp@al.educacao.sp.gov.br>
2024-06-05 08:51:08 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 400);
+  background("pink");
+}
+
+function draw() {
+  
+  stroke ("purple");
+  fill ("black");
+  
+  if(mouseIsPressed){
+  rect (mouseX,mouseY,20,20)
+  }
+}
